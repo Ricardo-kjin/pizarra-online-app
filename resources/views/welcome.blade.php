0d3ec4c3f4e264e785a2c6ca8256c06aeb716525
Rikrdo <rikrdoramirez1993@gmail.com>
2023-06-26 13:16:15 -0400

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <title>Pizarra Colaborativa</title>
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
